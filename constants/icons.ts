dd06e5be570374b41dc7ae368e11285613847719
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-08-25 23:31:13 +0100

    create and style the app nav

---
+++ constants/icons.ts
@@ -0,0 +1,5 @@
+import logo from "public/assets/svg/logo.svg";
+
+export const icons = {
+  logo,
+};
---
+++ src/components/app-nav.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AppNav() {
+  return (
+    <nav className=" bg-card p-2 rounded-xl w-full flex items-center justify-between">
+      <img src="/assets/svg/logo.svg" alt="" />
+      <div className=" bg-card-foreground p-2 rounded-lg">
+        <img src="public/assets/svg/icon-moon.svg" alt="" />
+      </div>
+    </nav>
+  );
+}
